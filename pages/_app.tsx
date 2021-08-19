755b24ed9a96b54631e8b2ae5601f863a93724b2
Roman Gladyshev <romagladyshev@gmail.com>
2021-08-19 14:43:19 +0600

    fix og images

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,13 +31,13 @@ function MyApp({ Component, pageProps }: AppProps) {
           description: 'Observer mission is to promote Web Vitals metrics in VC and startup markets. We use data from Google’s Pagespeed Insights API to get performance metrics for venture fund websites.',
           images: [
             {
-              url: 'https://raw.githubusercontent.com/real-festil/observer/main/public/ogLarge.png',
+              url: 'https://raw.githubusercontent.com/real-festil/observer-og/main/ogLarge.png',
               width: 800,
               height: 600,
               alt: 'Og Image Alt',
             },
             {
-              url: 'https://raw.githubusercontent.com/real-festil/observer/main/public/ogSmall.png',
+              url: 'https://raw.githubusercontent.com/real-festil/observer-og/main/ogSmall.png',
               width: 180,
               height: 180,
               alt: 'Og Image Alt',
